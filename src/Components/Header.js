6124e3401b398a98f11ee7488836189f379da4be
Nicole Anne Lopez <nicole.lopez@bax-shop.nl>
2019-07-05 14:18:47 +0800

    Updated contents

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="header">
         <ul className="header-link-group">
-            <HeaderLink linkName="Dev Profile" linkURL="http://nicoleannelopez.com"/>
+            <HeaderLink linkName="Dev Profile" linkURL="http://www.nicoleannelopez.com"/>
             <HeaderLink linkName="Personal Blog" linkURL="https://nicoleannelopez.wordpress.com"/>
         </ul>
     </div>
